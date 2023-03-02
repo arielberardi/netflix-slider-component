b0bc2907deea3cbf76175d453c9dddb151346fba
Ariel Berardi <ariel.berardi95@gmail.com>
2023-03-02 13:46:47 -0300

    Refactor data flow

--- src/components/CollectionSlider/CollectionSlider.jsx
+++ src/components/CollectionSlider/CollectionSlider.jsx
@@ -1,12 +1,15 @@
 import "./CollectionSlider.css";
 import PropTypes from "prop-types";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import PaginationBar from "../PaginationBar";
+import SliderArrow from "../SliderArrow";
 import Slider from "../Slider";
 
-function CollectionSlider({ title, width, data }) {
+function CollectionSlider({ title, width, tiles }) {
   const [currentPage, setCurrentPage] = useState(0);
-  const numberOfPages = data.length / width;
+  const [isMouseOver, setIsMouseOver] = useState(false);
+  const paginationRef = useRef();
+  const numberOfPages = Math.ceil(tiles.length / width);
 
   const generatePageBars = () => {
     const pageBars = [];
@@ -18,17 +21,53 @@ function CollectionSlider({ title, width, data }) {
     return pageBars;
   };
 
+  const limitRange = (value) => {
+    return Math.min(Math.max(value, 0), numberOfPages - 1);
+  };
+
+  const onSliderLeft = () => {
+    setCurrentPage(limitRange(currentPage - 1));
+  };
+
+  const onSliderRight = () => {
+    setCurrentPage(limitRange(currentPage + 1));
+  };
+
+  const onCollectionMouseOut = () => {
+    setIsMouseOver(false);
+    paginationRef.current.style.visibility = "hidden";
+  };
+
+  const onCollectionMouseOver = () => {
+    setIsMouseOver(true);
+    paginationRef.current.style.visibility = "visible";
+  };
+
   return (
-    <div className="collection-container">
+    <div
+      className="collection-container"
+      onMouseOver={onCollectionMouseOver}
+      onFocus={onCollectionMouseOver}
+      onMouseOut={onCollectionMouseOut}
+      onBlur={onCollectionMouseOut}
+    >
       <div className="collection-header">
         <h2 className="collection-title">{title}</h2>
-        <div className="collection-pagination">{generatePageBars()}</div>
+        <div className="collection-pagination" ref={paginationRef}>
+          {generatePageBars()}
+        </div>
       </div>
-      <div className="collection-body">
-        <Slider
-          collection={data}
-          pages={numberOfPages}
-          handlePageChange={(page) => setCurrentPage(page)}
+      <div className="slider-container">
+        <SliderArrow
+          direction="left"
+          handleClick={onSliderLeft}
+          hidden={currentPage === 0 || !isMouseOver}
+        />
+        <Slider tiles={tiles} page={currentPage} />
+        <SliderArrow
+          direction="right"
+          handleClick={onSliderRight}
+          hidden={currentPage >= numberOfPages - 1 || !isMouseOver}
         />
       </div>
     </div>
@@ -42,7 +81,7 @@ CollectionSlider.defaultProps = {
 CollectionSlider.propTypes = {
   title: PropTypes.string.isRequired,
   width: PropTypes.number,
-  data: PropTypes.arrayOf(Object).isRequired,
+  tiles: PropTypes.arrayOf(Object).isRequired,
 };
 
 export default CollectionSlider;
--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,58 +1,30 @@
 import "./Slider.css";
 import PropTypes from "prop-types";
-import { useState, useEffect, useRef } from "react";
-import SliderArrow from "../SliderArrow";
+import { useEffect, useRef } from "react";
 
-function Slider({ collection, pages, handlePageChange }) {
+function Slider({ tiles, page }) {
   const sliderRef = useRef();
-  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
-    sliderRef.current.style.setProperty("--slider-index", currentIndex);
-    handlePageChange(currentIndex);
-  }, [currentIndex]);
+    sliderRef.current.style.setProperty("--slider-index", page);
+  }, [page]);
 
   const generateTiles = () => {
-    return collection.map((item) => (
+    return tiles.map((item) => (
       <img src={item.img_url} key={item.id} alt={item.title} />
     ));
   };
 
-  const limitRange = (value) => {
-    return Math.min(Math.max(value, 0), pages - 1);
-  };
-
-  const onSliderLeft = () => {
-    setCurrentIndex(limitRange(currentIndex - 1));
-  };
-
-  const onSliderRight = () => {
-    setCurrentIndex(limitRange(currentIndex + 1));
-  };
-
   return (
-    <div className="slider-container">
-      <SliderArrow
-        direction="left"
-        handleClick={onSliderLeft}
-        hidden={currentIndex <= 0}
-      />
-      <div className="slider" ref={sliderRef}>
-        {generateTiles()}
-      </div>
-      <SliderArrow
-        direction="right"
-        handleClick={onSliderRight}
-        hidden={currentIndex >= pages - 1}
-      />
+    <div className="slider" ref={sliderRef}>
+      {generateTiles()}
     </div>
   );
 }
 
 Slider.propTypes = {
-  collection: PropTypes.arrayOf(Object).isRequired,
-  pages: PropTypes.number.isRequired,
-  handlePageChange: PropTypes.func.isRequired,
+  tiles: PropTypes.arrayOf(Object).isRequired,
+  page: PropTypes.number.isRequired,
 };
 
 export default Slider;
